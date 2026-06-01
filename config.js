afd6d66bf99a3bd8e04c9d64a0169860dbc928b3
Nguyễn Hoàng Tuấn <tuan1172005@gmail.com>
2026-06-01 21:02:57 +0700

    Add logging configuration constants to config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const LOG_ENDPOINT = "https://script.google.com/macros/s/AKfycbxyY79488SqzjwbZDJlHLVZiEJTPdpqHhd9La_ecaFtUIXc-TLgKLJ9QVKIr4T-vBU/exec";
+const LOG_KEY = "DAN_KEY_MOI_CUA_BAN_VAO_DAY"; // key bạn vừa đổi trong Code.gs
+
+const MATCH_THRESHOLD = 0.62;
+const LOG_SOURCE = "fpt-hr-faq-web";
