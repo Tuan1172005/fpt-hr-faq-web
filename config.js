19a61a3a541236c927f590cd2e03344b2087af7c
Nguyễn Hoàng Tuấn <tuan1172005@gmail.com>
2026-06-01 21:30:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const LOG_ENDPOINT = "https://script.google.com/macros/s/AKfycbxyY79488SqzjwbZDJlHLVZiEJTPdpqHhd9La_ecaFtUIXc-TLgKLJ9QVKIr4T-vBU/exec";
-const LOG_KEY = "DAN_KEY_MOI_CUA_BAN_VAO_DAY"; // key bạn vừa đổi trong Code.gs
+const LOG_KEY = "FPT_HR_2026_TUANNH82"; // key bạn vừa đổi trong Code.gs
 
 const MATCH_THRESHOLD = 0.62;
 const LOG_SOURCE = "fpt-hr-faq-web";
