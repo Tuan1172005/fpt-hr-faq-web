1d014e13dad9b6cc488c13127fb9d87300f6e8ed
Nguyễn Hoàng Tuấn <tuan1172005@gmail.com>
2026-06-05 22:23:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const LOG_ENDPOINT = "https://script.google.com/macros/s/AKfycbxyY79488SqzjwbZDJlHLVZiEJTPdpqHhd9La_ecaFtUIXc-TLgKLJ9QVKIr4T-vBU/exec";
+const LOG_ENDPOINT = "https://script.google.com/macros/s/AKfycbxhB7GUIeFhzDMg9wC3Xg9XwRCNOmVkckGT2tiBFIc5jwrog1q_9ygQouf7cTf0j-Q/exec";
 const LOG_KEY = "FPT_HR_2026_TUANNH82"; // key bạn vừa đổi trong Code.gs
 
 const MATCH_THRESHOLD = 0.62;
